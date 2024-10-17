8e349a11b8a229e1d1741325b9b9d22542bea77a
Edward Rudd <urkle@outoforder.cc>
2024-10-17 16:47:15 -0400

    Disable origin check

--- pkg/hookbot/util.go
+++ pkg/hookbot/util.go
@@ -10,6 +10,7 @@ import (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool { return true },
 }
 
 type WebsocketHandlerFunc func(*websocket.Conn, *http.Request)
